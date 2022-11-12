d4f52a528638b74e42f029388ad136d777f43dc6
alabh <alabenhmouda@gmail.com>
2022-11-12 08:36:55 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RainbowColorDirective } from './rainbow-color.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RainbowColorDirective
   ],
   imports: [
     BrowserModule
